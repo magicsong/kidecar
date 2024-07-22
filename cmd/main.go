3e50aadaf1fb64a60644a6fe4b8316eac4dba035
李志朋 <lizhipeng.629@bytedance.com>
2024-07-22 16:12:34 +0800

    sidecar by http

--- cmd/main.go
+++ cmd/main.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"io/ioutil"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/util/homedir"
-	"os"
+	"net/http"
 	"path/filepath"
-	ctrlLog "sigs.k8s.io/controller-runtime/pkg/log"
+	"strings"
 	"time"
 
-	"github.com/fsnotify/fsnotify"
-	"github.com/go-kit/log"
 	"github.com/magicsong/okg-sidecar/pkg"
 	kruisegameclientset "github.com/openkruise/kruise-game/pkg/client/clientset/versioned"
 	"github.com/prometheus/blackbox_exporter/config"
-	"github.com/prometheus/blackbox_exporter/prober"
 	"github.com/prometheus/client_golang/prometheus"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
 )
 
 var sc = config.NewSafeConfig(prometheus.DefaultRegisterer)
@@ -27,6 +25,7 @@ var sc = config.NewSafeConfig(prometheus.DefaultRegisterer)
 func main() {
 	// Load Kubernetes configuration
 	config, err := rest.InClusterConfig()
+	klog.Info("InClusterConfig")
 	if err != nil {
 		kubeconfig := filepath.Join(homedir.HomeDir(), ".kube", "config")
 		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
@@ -38,86 +37,47 @@ func main() {
 	// Create Kubernetes clientset
 	clientset := kubernetes.NewForConfigOrDie(config)
 	okgClientset := kruisegameclientset.NewForConfigOrDie(config)
-
 	gamePatch := pkg.NewGamePatcher(clientset, okgClientset)
-	// Get current GameServerSet information
-	gssName := os.Getenv("GAME_SERVER_SET_NAME")
-	namespace := os.Getenv("GAME_SERVER_SET_NAMESPACE")
-
-	// Create logger
-	logger := log.NewNopLogger()
-
-	if err = sc.ReloadConfig("blackbox.yml", logger); err != nil {
-		ctrlLog.FromContext(context.Background()).Error(err, "Error loading blackbox config")
-		panic(err.Error())
-	}
-	ctrlLog.Log.Info("blackbox config loaded, Modules: ", sc.C.Modules)
-
-	// Set up file watcher for config file
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		panic(err.Error())
-	}
-	defer watcher.Close()
-
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				if event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create {
-					err := sc.ReloadConfig("blackbox.yml", logger)
-					if err != nil {
-						ctrlLog.FromContext(context.Background()).Error(err, "Error loading blackbox config")
-					}
-					ctrlLog.Log.Info("blackbox config loaded, Modules: ", sc.C.Modules)
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				ctrlLog.FromContext(context.Background()).Error(err, "Error watching config file")
-			}
-		}
-	}()
-
-	err = watcher.Add("blackbox.yml")
-	if err != nil {
-		panic(err.Error())
-	}
 
 	// Start monitoring loop
 	for {
 		// Perform HTTP probe using Blackbox Exporter
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
-		// Get GameServer by gameserverset selector
-		gsList, err := gamePatch.ListGameServerByGSS(ctx, gssName, namespace)
-		if err != nil && errors.IsNotFound(err) {
-			ctrlLog.FromContext(ctx).Info("GameServer not found by gss", " gssName: ", gssName, " namespace: ", namespace)
+		// Get GameServer by probe selector
+		gsList, err := gamePatch.ListGameServersByProbeLabel(ctx)
+		if (err != nil && errors.IsNotFound(err)) || gsList == nil {
+			klog.FromContext(ctx).Error(err, "GameServer not found")
+			time.Sleep(10 * time.Second)
 			continue
 		}
-		ctrlLog.Log.Info("GameServer Info ", " len(gs): ", len(gsList.Items))
 
-		registry := prometheus.DefaultRegisterer.(*prometheus.Registry)
+		klog.Info("GameServer Info ", " len(gs): ", len(gsList.Items))
 		for _, gs := range gsList.Items {
 			// Probe GameServer Port By HTTP
-			ctrlLog.Log.Info("GameServer Probe", "gssName:", gs.Name, " gssNamespace:", gs.Namespace)
+			klog.Info("GameServer Probe", "gssName:", gs.Name, " gssNamespace:", gs.Namespace)
 			// Get HTTP RUL by gs
-			if url, ok := gs.Annotations["http-url"]; ok {
-				success := prober.ProbeHTTP(ctx, url, sc.C.Modules["http_2xx"], registry, logger)
-				ctrlLog.Log.Info("GameServer Probe", "gssName:", gs.Name, " gssNamespace:", gs.Namespace, "success:", success)
+			podIp := gs.Status.PodStatus.PodIP
+			podPort := gs.Labels[pkg.GMAESERVER_PROBE_PORT_LABEL]
+			wantResp := gs.Labels[pkg.GAMESERVER_WANT_RESP]
+			klog.Info("wantResp: ", wantResp)
+
+			if podPort != "" && wantResp != "" {
+				url := "http://" + podIp + ":" + podPort
+				err, success := sendHTTP(url, wantResp)
+				if err != nil {
+					klog.Error(err, "falied to send http", " url is ", url)
+				}
+				klog.Info("GameServer Probe", " gsName:", gs.Name, " gsNamespace:", gs.Namespace, " url", url, " success:", success)
 				if success && gs.Spec.OpsState != "Allocated" {
 					gs.Spec.OpsState = "Allocated"
 					if err = gamePatch.PatchGameServer(ctx, &gs); err != nil {
-						fmt.Printf("Error patching GameServer status: %v\n", err)
+						klog.Error("Error patching GameServer status: %v\n", err)
 					}
-				} else if !success && gs.Spec.OpsState != "WaitToBeDeleted" {
+				} else if !success && gs.Spec.OpsState == "Allocated" {
 					gs.Spec.OpsState = "WaitToBeDeleted"
 					if err = gamePatch.PatchGameServer(ctx, &gs); err != nil {
-						fmt.Printf("Error patching GameServer status: %v\n", err)
+						klog.Error("Error patching GameServer status: %v\n", err)
 					}
 				}
 			}
@@ -125,6 +85,39 @@ func main() {
 		}
 
 		// Sleep for a while before next iteration
-		time.Sleep(100 * time.Second)
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func sendHTTP(url, wantStr string) (error, bool) {
+	resp, err := http.Get(url)
+	if err != nil {
+		klog.Error(err, "http get error")
+		return err, false
+	}
+	defer resp.Body.Close()
+
+	klog.Info("http status code is: ", resp.StatusCode)
+	// 检查HTTP响应状态码
+	if resp.StatusCode != http.StatusOK {
+		return nil, false
+	}
+
+	// 读取响应的body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		klog.Error(err, "read http body error")
+		return err, false
+	}
+
+	// 将响应的body转为字符串
+	bodyString := string(body)
+
+	// 检查响应内容中是否包含"idle"
+	klog.Info("http bodyString: ", bodyString)
+	if strings.Contains(bodyString, wantStr) {
+		return nil, true
+	} else {
+		return nil, false
 	}
 }
