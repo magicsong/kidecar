5f760b47b6e7e4a4ea04655841b4b5c5a8259ec6
magicsong <songxuetao@bytedance.com>
2024-10-10 16:27:03 +0800

    add injector

---
+++ cmd/injector/main.go
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"text/template"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/jessevdk/go-flags"
+	"github.com/salesforce/generic-sidecar-injector/pkg/injectionwebhook"
+	"github.com/salesforce/generic-sidecar-injector/pkg/injectionwebhook/config"
+	"github.com/salesforce/generic-sidecar-injector/pkg/metrics"
+	"github.com/salesforce/generic-sidecar-injector/pkg/mutationconfig"
+	"github.com/salesforce/generic-sidecar-injector/pkg/sidecarconfig"
+	"github.com/salesforce/generic-sidecar-injector/pkg/util"
+	"github.com/spf13/afero"
+)
+ 
+ const (
+	 successExitCode = 0
+	 errorExitCode   = 1
+ )
+ 
+ func main() {
+	 webhookConfig, err := config.NewWebhookConfig()
+	 if err != nil {
+		 fmt.Fprintf(os.Stderr, "api=main, reason=config.NewWebhookConfig, err=%v\n", err)
+		 os.Exit(errorExitCode)
+	 }
+	 if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+		 os.Exit(successExitCode)
+	 }
+ 
+	 if webhookConfig.BuildInfoLabels != "" {
+		 metrics.WebhookBuildInfo(webhookConfig.BuildInfoLabels)
+	 }
+ 
+	 // Sanity check for templateFile. Eligible fields https://salesforce.quip.com/pEW6A6AtpwRc
+	 // TODO: move above doc to git wiki
+	 sidecarConfigTemplate, err :=
+		 template.New(filepath.Base(webhookConfig.SidecarConfigFile)).
+			 Delims(util.TemplateLeftDelimiter, util.TemplateRightDelimiter).
+			 Funcs(sidecarconfig.SidecarTemplateExtraFuncs()).
+			 ParseFiles(webhookConfig.SidecarConfigFile)
+	 if err != nil {
+		 glog.Errorf("api=main, reason=template.New, file=%q, err=%v", webhookConfig.SidecarConfigFile, err)
+		 os.Exit(errorExitCode)
+	 }
+ 
+	 err = sidecarconfig.TemplateSanityCheck(sidecarConfigTemplate)
+	 if err != nil {
+		 glog.Errorf("api=main, reason=sidecarconfig.TemplateSanityCheck, message=template may contain ineligible fields, err=%v", err)
+		 os.Exit(errorExitCode)
+	 }
+ 
+	 mutationConfigs, err := mutationconfig.NewMutatingConfigs(webhookConfig.MutationConfigFile)
+	 if err != nil {
+		 glog.Errorf("api=main, reason=mutationconfig.NewMutatingConfig, file=%q, err=%v", webhookConfig.MutationConfigFile, err)
+		 os.Exit(errorExitCode)
+	 }
+ 
+	 fs := afero.NewOsFs()
+	 certReloader := util.NewCertificatePKIReloaderFull(fs, webhookConfig.CertFilePath, webhookConfig.KeyFilePath, time.Minute*15)
+ 
+	 whsrv := injectionwebhook.NewWebhookServer(webhookConfig, sidecarConfigTemplate, mutationConfigs, certReloader)
+	 if err != nil {
+		 glog.Errorf("api=main, reason=injectionwebhook.NewWebhookServer, err=%v", err)
+		 os.Exit(errorExitCode)
+	 }
+	 doneListeningTLSChannel, doneListeningHTTPChannel, err := whsrv.Start()
+	 if err != nil {
+		 glog.Errorf("api=main, reason=whsrv.Start, err=%v", err)
+		 os.Exit(errorExitCode)
+	 }
+ 
+	 // listening OS shutdown signal
+	 signalChan := make(chan os.Signal, 1)
+	 signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+ 
+	 stopped := false
+	 // Wait until we receive either a termination signal, or the server stops by itself for some reason
+	 select {
+	 case signal := <-signalChan:
+		 {
+			 glog.Infof("Received a termination signal. SIG=%s", signal)
+		 }
+	 case stopped = <-doneListeningTLSChannel:
+		 {
+			 glog.Warning("TLS Server has stopped on it's own... exiting.")
+		 }
+	 case stopped = <-doneListeningHTTPChannel:
+		 {
+			 glog.Warning("HTTP Server has stopped on it's own... exiting.")
+		 }
+	 }
+ 
+	 if !stopped {
+		 whsrv.Stop()
+	 }
+ 
+	 glog.Info("Webhook server exited successfully.")
+	 os.Exit(successExitCode)
+ }
--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.22.2
 
 require (
 	github.com/agiledragon/gomonkey/v2 v2.12.0
+	github.com/golang/glog v1.2.2
+	github.com/jessevdk/go-flags v1.6.1
 	github.com/prometheus/blackbox_exporter v0.25.0
 	github.com/prometheus/client_golang v1.19.1
+	github.com/salesforce/generic-sidecar-injector v0.0.0-20231023210411-3a79d8dbf547
+	github.com/spf13/afero v1.11.0
 	github.com/tidwall/gjson v1.17.3
 	gomodules.xyz/jsonpatch/v2 v2.4.0
 	k8s.io/apimachinery v0.31.0
@@ -17,6 +21,7 @@ require (
 	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logr/zapr v1.3.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
@@ -65,13 +70,13 @@ require (
 	github.com/prometheus/common v0.55.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/spf13/pflag v1.0.5
-	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/mod v0.17.0
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/oauth2 v0.21.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/term v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
